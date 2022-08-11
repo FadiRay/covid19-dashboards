6825bb3cd84d66c0e79e9ce8e6920fb5124578e5
FadiRay <104934882+FadiRay@users.noreply.github.com>
2022-08-11 17:50:39 +0200

    Add files via upload

--- test_covid19_dashboards.py
+++ test_covid19_dashboards.py
@@ -1,3 +1,10 @@
-def test_it_works():
-  pass
+from covid19 import conn, SelectedDate, SelectedCountry, SelectedParameter
 
+def test_connection():
+    assert conn()[0]==200, 'no response from the covid19 data'
+    assert conn()[1]==200, 'no response from geojson'
+    assert SelectedDate()==True, 'Error by selecting the date'
+    assert SelectedCountry()==True, 'Error by selecting the country'
+    assert SelectedParameter()==True, 'Error by selecting Cases/Deaths'
+    
+    
